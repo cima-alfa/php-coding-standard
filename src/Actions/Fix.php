e55af3d778c25a5cfe323baf1b7f660cf491121b
cima-alfa <cima.alfa.business@gmail.com>
2025-07-01 21:45:17 +0200

    Add Fix Action

---
+++ src/Actions/Fix.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CimaAlfaCSFixers\Actions;
+
+use PhpCsFixer\Error\ErrorsManager;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
+final class Fix
+{
+    private ErrorsManager $errorsManager;
+    private EventDispatcher $eventDispatcher;
+
+    public function __construct()
+    {
+        $this->errorsManager = new ErrorsManager;
+        $this->eventDispatcher = new EventDispatcher;
+    }
+}
