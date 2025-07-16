eeb3ac6f6dd1cb317354100085f92c5bf0385cc6
jeremy-step <119798452+jeremy-step@users.noreply.github.com>
2025-07-16 23:25:09 +0200

    TODO: Decide whether this is needed

--- src/Action/Fix.php
+++ src/Action/Fix.php
@@ -4,17 +4,9 @@ declare(strict_types=1);
 
 namespace CimaAlfaCSFixers\Action;
 
-use PhpCsFixer\Error\ErrorsManager;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-
 final class Fix
 {
-    private ErrorsManager $errorsManager;
-    private EventDispatcher $eventDispatcher;
-
     public function __construct()
     {
-        $this->errorsManager = new ErrorsManager;
-        $this->eventDispatcher = new EventDispatcher;
     }
 }
